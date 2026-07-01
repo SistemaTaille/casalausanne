a311bb92d21a48a84597b2e4f048962ce6b69691
Pedro Reuter <pedroreuter.taille@gmail.com>
2026-07-01 10:10:40 -0300

    Nomenclatura

--- src/routes/lausanne.tsx
+++ src/routes/lausanne.tsx
@@ -236,7 +236,7 @@ function Index() {
               <p>
                 Mais que uma residência, um convite para viver com exclusividade, contemplação e qualidade de vida.
                 São 328,18 m² de área equivalente, distribuídos em dois pavimentos, com pé-direito duplo no living,
-                três suítes amplas, escritório e integração total entre os ambientes sociais e a área externa com
+                quatro suítes amplas, escritório e integração total entre os ambientes sociais e a área externa com
                 piscina.
               </p>
             </div>
